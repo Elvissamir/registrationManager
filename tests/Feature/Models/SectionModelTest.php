beea8ab94e32d589f939469b94b1a01d71118404
ELvis <elvissamir18@gmail.com>
2021-10-14 14:48:32 -0400

    Created Servm Project. A registration manager system.

---
+++ tests/Feature/Models/SectionModelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SectionModelTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_a_section_belongs_to_many_courses()
+    {
+        
+    }
+}
