27267fdb22300ae5c6fadf319ace7752194ab5a9
Elvis Carrasco <elvissamir18@gmail.com>
2022-03-16 13:56:13 -0400

    Modified AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,6 +32,8 @@ class AppServiceProvider extends ServiceProvider
 
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
+            $this->app['request']->server->set('HTTPS','on');
         }
+
     }
 }
